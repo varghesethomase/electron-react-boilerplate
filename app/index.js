e94b046616d76b747f87b149e75dd8aa3f30a32e
Varghese Thomas <varghesethomase@gmail.com>
2017-03-05 22:39:46 +0530

    Fixing message passing in application thread

--- app/index.js
+++ app/index.js
@@ -3,12 +3,19 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import { ipcRenderer } from 'electron';
+import { Socket } from 'electron-ipc-socket';
 import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.css';
 
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
+
+// Configuring the connection to main thread from application thread
+const socket = Socket('etainer-main', ipcRenderer);
+socket.open();
+socket.send('ready');
 
 render(
   <Provider store={store}>
